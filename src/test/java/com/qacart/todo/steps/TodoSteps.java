cd574669ae08ee9981e3b3174989337fae0ebb85
mmetehri <mokhtar.metehri@yahoo.fr>
2024-06-14 20:23:57 +0200

    Add step files (java)

---
+++ src/test/java/com/qacart/todo/steps/TodoSteps.java
@@ -0,0 +1,22 @@
+package com.qacart.todo.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TodoSteps {
+
+    @Given("User is in the todo page")
+    public void isUserInTodoPage(){
+
+    }
+
+    @When("User adds a new todo")
+    public void AddNewTodo(){
+
+    }
+    @Then("todo should be added correctly")
+    public void TodoShouldBeAddedCorrectly(){
+
+    }
+}
---
+++ src/test/java/com/qacart/todo/steps/UserSteps.java
@@ -0,0 +1,23 @@
+package com.qacart.todo.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserSteps {
+
+    @Given("User is at the login page")
+    public void isUserAtTheLoginPage(){
+
+    }
+
+    @When("User fills the email and password and login")
+    public void UserFillsEmailAndPassword(){
+
+    }
+
+    @Then("Welcome message should be displayed")
+    public void IsWelcomeMessageDisplayed(){
+
+    }
+}
